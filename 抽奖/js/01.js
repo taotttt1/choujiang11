9b4129a54026b8a0ecb901be3f3998ded39e132f
taottt1 <824422831@qq.com>
2022-06-08 12:08:03 +0800

    3

--- 抽奖/js/01.js
+++ 抽奖/js/01.js
@@ -1,26 +1,38 @@
 window.onload = function(){
-	var btn = document.querySelector(".btn");
-	var obox = document.querySelector(".obox");
-	var second = document.querySelector(".second");
-	var huodon = document.querySelector(".huodon");
-	var tishi = document.querySelector(".tishi");
-	var queding = document.querySelector(".queding");
 	var kaishi = document.querySelector(".kaishi");
-	var arr = ["黄忠村酒2瓶","获得莱沃口腔储值卡","中国联通流量卡","获得S5健身俱乐部贵宾卡","获得圣雅瑜伽SPA会所储值卡","获得陶进二胡工作室","获得蛋壳科技VR体验卡"]
-	btn.onclick = function(){ // 跳到转盘页面
-	obox.style.display="none";
-	second.style.display="block";
-	}
+	var arr = ["获得黄忠村酒2瓶","获得莱沃口腔储值卡","获得中国联通流量卡","获得S5健身俱乐部贵宾卡","获得圣雅瑜伽SPA会所储值卡","获得陶进二胡工作室","获得蛋壳科技VR体验卡"]
+	var box4 = document.querySelector(".box4");
+	var quxiao = document.querySelector(".quxiao");
+	var name = document.querySelector(".name");
+	var phone = document.querySelector(".phone");
+	var box4p2 = document.querySelector(".box4-p2");
+	var guize = document.querySelector(".guize");
+	var box5p = document.querySelector(".box5-p");
+	var box6 = document.querySelector(".box6");
+	var re1 = /^[a-z][a-z0-9_-]{5,19}$/; // 姓名
 	
-	// 活动规则
-	huodon.onclick = function(){  
-	tishi.style.display="block";
-	queding.onclick = function(){
-		tishi.style.display="none";
-	}
-	}
-	// 抽奖转盘
-	kaishi.onclick = function(){ // 收集信息
+	// 规则
+	guize.onclick = function(){
+		box6.style.display="block";	
+		
+		box5p.onclick = function(){
+			box6.style.display="none";	
+		}
 		
 	}
+	
+	
+	
+	// 抽奖转盘
+	kaishi.onclick = function(){ // 收集信息
+		box4.style.display="block";		
+
+		
+		// 点击取消
+		quxiao.onclick = function(){
+			box4.style.display="none";
+		}
+		    
+	}
+	
 }
